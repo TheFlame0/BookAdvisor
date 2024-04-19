32a018def2092f93d5b3bacbe2b8f17e270551ed
Hamza Ba'aqeel <h.m.b.2002@hotmail.com>
2024-04-20 02:30:40 +0300

    Update image source paths to use absolute paths; fix relative paths
    Changes made to image source paths in various components to use absolute paths instead of relative paths. This should ensure that images are loaded correctly regardless of the component's location. Affect

--- react-app-BookAdvisor/src/HomePage-Components/BookShelf.jsx
+++ react-app-BookAdvisor/src/HomePage-Components/BookShelf.jsx
@@ -10,7 +10,7 @@ const BookShelf = () => {
                 <form className="search-bar">
                     <input id="searchbar" type="text" placeholder="  Search books..." />
                     <button type="submit">
-                        <img id="buttonImage" src="src/imgs/search-interface-symbol.png" alt="Search Button"/>
+                        <img id="buttonImage" src="/src/imgs/search-interface-symbol.png" alt="Search Button"/>
                     </button>
                 </form> 
 
@@ -21,22 +21,22 @@ const BookShelf = () => {
 
                 <div className="book-container">
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
 
 
@@ -48,23 +48,23 @@ const BookShelf = () => {
                 </div>
 
                 <div className="book-container">
-                <NavButton imageSrc={"src/imgs/left-arrow.png"} altText={"Next Book Button"} />
+                <NavButton imageSrc={"/src/imgs/left-arrow.png"} altText={"Next Book Button"} />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />    
-                                    <NavButton imageSrc={"src/imgs/right-arrow.png"} altText={"Next Book Button"} />
+                                    <NavButton imageSrc={"/src/imgs/right-arrow.png"} altText={"Next Book Button"} />
                </div>
 
                 <div className="section">
@@ -74,21 +74,21 @@ const BookShelf = () => {
                 <div className="book-container">
                       
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />
                                     <Book
-                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="src/imgs/Book8.jpeg" altText="Book 11"
+                                        title="The Science of Selling" author="David Hoffeld" rating="4.2/5" tags={["History", "Data", "English"]} description="Marco, a disillusioned backpacker in his late 20s, embarks on a solitary journey in search for meaning." imageSrc="/src/imgs/Book8.jpeg" altText="Book 11"
                                     />  
-                                    {/* <NavButton imageSrc={"src/imgs/right-arrow.png"} altText={"Next Book Button"} /> */}
+                                    {/* <NavButton imageSrc={"/src/imgs/right-arrow.png"} altText={"Next Book Button"} /> */}
 
                                     
                 </div>
--- react-app-BookAdvisor/src/HomePage-Components/Discover.jsx
+++ react-app-BookAdvisor/src/HomePage-Components/Discover.jsx
@@ -6,19 +6,19 @@ return (
                   <table className="book-table">
                         <tr>
                         <td className="empty-space animation"></td>
-                        <td><img className="animation animation-delay-1s" src="src\imgs\Book1.jpeg" alt="Book 1"></img></td>
-                        <td><img className="animation animation-delay-2s" src="src\imgs\Book2.jpeg" alt="Book 2"></img></td>
-                        <td><img className="animation animation-delay-3s" src="src\imgs\Book3.jpeg" alt="Book 3"></img></td>
-                        <td><img className="animation animation-delay-4s" src="src\imgs\Book4.jpeg" alt="Book 4"></img></td>
-                        <td><img className="animation animation-delay-5s" src="src\imgs\Book5.jpeg" alt="Book 5"></img></td>
+                        <td><img className="animation animation-delay-1s" src={"/src/imgs/Book1.jpeg"} alt="Book 1"></img></td>
+                        <td><img className="animation animation-delay-2s" src={"/src/imgs/Book2.jpeg"} alt="Book 2"></img></td>
+                        <td><img className="animation animation-delay-3s" src={"/src/imgs/Book3.jpeg"} alt="Book 3"></img></td>
+                        <td><img className="animation animation-delay-4s" src={"/src/imgs/Book4.jpeg"} alt="Book 4"></img></td>
+                        <td><img className="animation animation-delay-5s" src={"/src/imgs/Book5.jpeg"} alt="Book 5"></img></td>
                         </tr>
                         <tr>
                         <td className="empty-space"></td> 
-                        <td><img className="animation animation-delay-1s" src="src\imgs\Book11.png" alt="Book 6"></img></td>
-                        <td><img className="animation animation-delay-2s" src="src\imgs\Book7.jpeg" alt="Book 7"></img></td>
-                        <td><img className="animation animation-delay-3s" src="src\imgs\Book8.jpeg" alt="Book 8"></img></td>
-                        <td><img className="animation animation-delay-4s" src="src\imgs\Book9.jpeg" alt="Book 9"></img></td>
-                        <td><img className="animation animation-delay-5s" src="src\imgs\Book10.jpeg" alt="Book 10"></img></td>
+                        <td><img className="animation animation-delay-1s" src={"/src/imgs/Book11.png"} alt="Book 6"></img></td>
+                        <td><img className="animation animation-delay-2s" src={"/src/imgs/Book7.jpeg"} alt="Book 7"></img></td>
+                        <td><img className="animation animation-delay-3s" src={"/src/imgs/Book8.jpeg"} alt="Book 8"></img></td>
+                        <td><img className="animation animation-delay-4s" src={"/src/imgs/Book9.jpeg"} alt="Book 9"></img></td>
+                        <td><img className="animation animation-delay-5s" src={"/src/imgs/Book10.jpeg"} alt="Book 10"></img></td>
                         </tr>
                   </table>
             </div>
