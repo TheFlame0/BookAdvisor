ab534326185f71c32406e2304ae60a16bf67fb05
Hassan-Alsharyufi <h.alsharyoufi@gmail.com>
2024-04-20 23:45:59 +0300

    continue on sign up

--- react-app-BookAdvisor/src/pages/SignUp.jsx
+++ react-app-BookAdvisor/src/pages/SignUp.jsx
@@ -1,7 +1,18 @@
 const SignUp = () =>{
     return(
         <>
-        <h1>Book Advisor</h1>
+        <header><h1>Book Advisor</h1>
+        <img className="logo" src={"/src/imgs/logoBookAdvisor.jpeg"} alt="logo" height="50" width="50"></img>
+        </header>
+        <body>
+            <h1>Create account</h1>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <button></button>
+        </body>
         </>
             
         
