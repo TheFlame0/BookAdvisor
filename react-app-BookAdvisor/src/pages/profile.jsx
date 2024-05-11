528e25a58a188d0e82f183d0405f1d37caea33df
Hamza Ba'aqeel <h.m.b.2002@hotmail.com>
2024-05-11 21:33:19 +0300

    Test Deployment

--- react-app-BookAdvisor/src/pages/profile.jsx
+++ react-app-BookAdvisor/src/pages/profile.jsx
@@ -12,7 +12,7 @@ const UserProfile = () => {
             <div className='container-name-img'>
               <div className="imgProfile-container">
                 <img src="/src/imgs/prifile-img-cartoon.png" alt="Profile" className="imgProfile"/>
-                <p>Hamza Baaqil</p>
+                <p>Hamza Baaqil </p>
               </div>
             </div>
           </div>
